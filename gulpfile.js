0453a2a1ae2071bfcbef66ed670135c50ce28172
designhorf <designhorf@gmail.com>
2015-07-04 11:38:30 +0200

    added autoprefixer, ruby-sass and sourcemap to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-ruby-sass');
 var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', function () {
-  gulp.src('stylesheets/**/*.scss')
-    .pipe(sass({
-        errLogToConsole: true
+gulp.task('sass', function () {
+  return sass('stylesheets/**/*.scss', { sourcemap: true })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
-    .pipe(gulp.dest('./assets/stylesheets/'));
+
+    .pipe(sourcemaps.write('maps', {
+        includeContent: false,
+        sourceRoot: 'stylesheets/**/*.scss'
+    }))
+
+    .pipe(gulp.dest('./assets/'));
 });
 
 //Watch
 gulp.task('watch', function () {
-  gulp.watch('stylesheets/**/*.scss', ['styles']);
+  gulp.watch('stylesheets/**/*.scss', ['sass']);
 });
 
+//Default
+// gulp.task('default', function () {
+  
+// });
 
