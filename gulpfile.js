54d52e1dd9feaaf3990a8f8d7ef43fd277b8ccf6
designhorf <designhorf@gmail.com>
2015-07-04 10:49:18 +0200

    modified package.json / added sass to gulp // deleted unused files

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
-gulp.task('default', function() {
-  return gulp.src('../stylesheets/*.scss')
-    // .pipe(watch('../stylesheets/*.scss'))
-    .pipe(sass())
-    .pipe(gulp.dest('dist'));
+gulp.task('styles', function () {
+  gulp.src('stylesheets/**/*.scss')
+    .pipe(sass({
+        errLogToConsole: true
+    }))
+    .pipe(gulp.dest('./assets/stylesheets/'));
 });
+
